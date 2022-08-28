34dbb99167c3270e0bb788d482e8c7e3e8e92f22
Chris Allen <58678379+Skelt3r@users.noreply.github.com>
2022-08-28 15:31:04 -0500

    Add configuration functionality

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 
 class ConwayTk:
-    def __init__(self, columns: int=48, rows: int=24, interval: int=100, random: bool=False):
+    def __init__(self, columns: int=48, rows: int=24, interval: int=100, live_color: str='white', dead_color: str='black', random: bool=False):
         """
         Accept configuration settings and initialize base variables.
 
@@ -18,10 +18,12 @@ class ConwayTk:
         self.rows = rows
         self.columns = columns
         self.interval = interval
+        self.live_color = live_color
+        self.dead_color = dead_color
         self.data_array = self.create_2d_array(random=random)
         self.button_array = self.create_2d_array(value=None)
         self.paused = True
-        self.colors = ['black', 'white', 'red', 'green', 'blue', 'yellow', 'limegreen', 'cyan', 'magenta', 'purple', 'brown']
+        self.colors = ['black', 'white', 'red', 'green', 'blue', 'yellow', 'lime green', 'cyan', 'magenta', 'purple', 'brown']
     
 
     def create_2d_array(self, value: None|int=0, random: bool=False):
@@ -49,9 +51,9 @@ class ConwayTk:
         for x in range(self.rows):
             for y in range(self.columns):
                 if self.data_array[y][x] == 0:
-                    button = Button(self.grid_frame, bg='black')
+                    button = Button(self.grid_frame, bg=self.dead_color)
                 else:
-                    button = Button(self.grid_frame, bg='white')
+                    button = Button(self.grid_frame, bg=self.live_color)
 
                 button.config(height=2, width=4, bd=1, relief='solid', command=lambda x=x, y=y, b=button: self.click(x, y, b))
                 button.grid(row=x, column=y)
@@ -87,10 +89,10 @@ class ConwayTk:
         """
         if button['bg'] == 'white':
             self.data_array[y][x] = 0
-            button['bg'] = 'black'
+            button['bg'] = self.dead_color
         else:
             self.data_array[y][x] = 1
-            button['bg'] = 'white'
+            button['bg'] = self.live_color
     
 
     def save_pattern(self):
@@ -114,6 +116,16 @@ class ConwayTk:
 
     def configure(self):
         """Configure the game parameters."""
+        def apply():
+            self.rows = num_rows.get()
+            self.columns = num_cols.get()
+            self.interval = int_ms.get()
+            self.live_color = live_color.get()
+            self.dead_color = dead_color.get()
+            self.root.destroy()
+            self.__init__(self.columns, self.rows, self.interval, self.live_color, self.dead_color, True)
+            self.run()
+
         win = Toplevel(self.root)
         win.title('Settings')
 
@@ -126,16 +138,16 @@ class ConwayTk:
         num_rows = IntVar(win, 24)
         num_cols = IntVar(win, 48)
         int_ms = IntVar(win, 100)
-        live_color = StringVar(win, 'white')
-        dead_color = StringVar(win, 'black')
+        live_color = StringVar(win, self.live_color.title())
+        dead_color = StringVar(win, self.dead_color.title())
 
-        rows_box = Spinbox(win, textvariable=num_rows, values=list(range(2, 101)), validate='all')
-        cols_box = Spinbox(win, textvariable=num_cols, values=list(range(2, 101)), validate='all')
-        interval_box = Spinbox(win, textvariable=int_ms, values=list(range(1, 5001)), validate='all')
-        live_color_menu = OptionMenu(win, live_color, *self.colors, command=None)
-        dead_color_menu = OptionMenu(win, dead_color, *self.colors, command=None)
+        rows_box = Spinbox(win, textvariable=num_rows, from_=2, to=100)
+        cols_box = Spinbox(win, textvariable=num_cols, from_=2, to=100)
+        interval_box = Spinbox(win, textvariable=int_ms, from_=1, to=5000)
+        live_color_menu = OptionMenu(win, live_color, *self.colors, command=lambda _: live_color.set(live_color.get().title()))
+        dead_color_menu = OptionMenu(win, dead_color, *self.colors, command=lambda _: dead_color.set(dead_color.get().title()))
 
-        apply_button = Button(win, text='Apply', command=None)
+        apply_button = Button(win, text='Apply', command=apply)
 
         rows_label.grid(row=0, column=0)
         cols_label.grid(row=1, column=0)
@@ -188,10 +200,10 @@ class ConwayTk:
                         neighbors = self.get_neighbors(x, y)
 
                         if state == 0 and neighbors == 3:
-                            self.button_array[y][x].config(bg='white')
+                            self.button_array[y][x].config(bg=self.live_color)
                             next_cycle[y][x] = 1
                         elif state == 1 and (neighbors < 2 or neighbors > 3):
-                            self.button_array[y][x].config(bg='black')
+                            self.button_array[y][x].config(bg=self.dead_color)
                             next_cycle[y][x] = 0
                         else:
                             next_cycle[y][x] = state
@@ -219,10 +231,10 @@ class ConwayTk:
         self.file_menu.add_command(label='Save Pattern', command=self.save_pattern, accelerator='|   S')
         self.file_menu.add_command(label='Load Pattern', command=self.load_pattern, accelerator='|   L')
         self.file_menu.add_separator()
+        self.file_menu.add_command(label='Configure', command=self.configure, accelerator='|   F')
         self.file_menu.add_command(label='Pause', command=self.pause, accelerator='|   Space')
         self.file_menu.add_command(label='Reset', command=self.reset, accelerator='|   R')
         self.file_menu.add_command(label='Clear', command=self.clear, accelerator='|   C')
-        self.file_menu.add_command(label='Configure', command=self.configure, accelerator='|   F')
 
         self.bg_frame.pack(expand=True, fill='both')
         self.grid_frame.pack(side='top', anchor='c')
@@ -235,12 +247,12 @@ class ConwayTk:
         self.root.bind_all('S', lambda _: self.save_pattern())
         self.root.bind_all('l', lambda _: self.load_pattern())
         self.root.bind_all('L', lambda _: self.load_pattern())
+        self.root.bind_all('f', lambda _: self.configure())
+        self.root.bind_all('F', lambda _: self.configure())
         self.root.bind_all('r', lambda _: self.reset())
         self.root.bind_all('R', lambda _: self.reset())
         self.root.bind_all('c', lambda _: self.clear())
         self.root.bind_all('C', lambda _: self.clear())
-        self.root.bind_all('f', lambda _: self.configure())
-        self.root.bind_all('F', lambda _: self.configure())
 
         self.root.focus_force()
         self.root.config(menu=self.menu_bar)
